225ee03ecf2dcd05bdc0c3668c548dd4a2a68cb9
Grace Egbo <gegbo@fb.com>
2016-06-15 14:40:57 -0700

    Completed required component

---
+++ app/src/main/java/com/example/gegbo/filxsterapp/adapters/MovieArrayAdapter.java
@@ -0,0 +1,54 @@
+package com.example.gegbo.filxsterapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.gegbo.filxsterapp.R;
+import com.example.gegbo.filxsterapp.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by gegbo on 6/15/16.
+ */
+public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    public MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1,movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //get the data item for position
+        Movie movie = getItem(position);
+
+        //check to see if existing view is being reused
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie,parent, false);
+        }
+
+        //find the image view
+        ImageView ivImage = (ImageView)convertView.findViewById(R.id.ivMovieImage);
+        //clear out image from convertView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView)convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView)convertView.findViewById(R.id.tvOverview);
+
+        //populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        //return the view
+        return convertView;
+
+    }
+}
